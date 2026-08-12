94691633a2cc4f1c7bc445a1022470e280c1e957
Michal Romero <michal.romero02@gmail.com>
2026-08-12 02:04:05 +0100

    add character animation flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 5;
+    public int facingDirection = 1;
+
     public Rigidbody2D rb;
     public Animator anim;
 
@@ -16,6 +18,21 @@ public class PlayerMovement : MonoBehaviour
         //left = -1 , right = +1 , no input = 0
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        //if we are pressing right but facing left OR pressing left but facing right
+        //transform.localScale.x is what you find in transform where you can set the orientation of the player
+        if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+            
+        void Flip()
+        {
+            //flips facing direction
+            facingDirection *= -1;
+            // *= -1 doesnt work here as you cant modify a single aspect of the local scale, so you have to modify all 3 
+            transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
 
         //sets animators floats to mirror our button presses
         //Mathf.Abs turn numbers into absolute (+ve numbers), so moving left will laso be positive
