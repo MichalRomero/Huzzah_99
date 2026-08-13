adf74538d7ebca160232352be89ce1601af8036c
Michal Romero <michal.romero02@gmail.com>
2026-08-13 22:04:12 +0100

    diplay health UI and add animation when chaniging health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
@@ -7,11 +8,27 @@ public class PlayerHealth : MonoBehaviour
     public int currentHealth;
     public int maxHealth;
 
+    public TMP_Text HealthText;
+    public Animator healthTextAnim;
+
+    private void Start()
+    {
+        //used for health UI
+        HealthText.text = "HP: " + currentHealth + "/" + maxHealth;
+    }
+
+    //amount allows to pass in healing or damage amounts
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
 
-        //if characters health is below zero, die. (right njow this turns off the player obj)
+        //when player takes damage, play TextUpdate Animation
+        healthTextAnim.Play("TextUpdate");
+
+        //updates health UI
+        HealthText.text = "HP: " + currentHealth + "/" + maxHealth;
+
+        //if characters health is below zero, die. (right now this turns off the player obj)
         if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
