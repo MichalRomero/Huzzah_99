597bf940e15c195a26497cfa41ea98f51f5372ad
Michal Romero <michal.romero02@gmail.com>
2026-08-14 14:18:36 +0100

    flip enemy sprite depending where the player is.

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -1,10 +1,12 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Enemy_Movement : MonoBehaviour
 {
     public float speed;
-
-    private bool isChasing;
+    private bool isChasing = true;
+    //facing right
+    private int facingDirection = 1;
 
     private Rigidbody2D rb;
     public Transform player;
@@ -13,17 +15,34 @@ public class Enemy_Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        isChasing = true;
     }
 
     // Update is called once per frame
     void Update()
+{
+    if (isChasing)
     {
-        if(isChasing == true)
+        //if plaeyrs is on right side of enemy and enemy facing left, then flip
+        if (player.position.x > transform.position.x && facingDirection == -1)
         {
-            Vector2 direction = (player.position - transform.position).normalized;
-            rb.linearVelocity = direction * speed;
+            Flip();
         }
-        
+        //if plaeyrs is on left side of enemy and enemy facing right, then flip
+        else if (player.position.x < transform.position.x && facingDirection == 1)
+        {
+            Flip();
+        }
+
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.linearVelocity = direction * speed;
     }
+}
+
+    void Flip()
+        {
+            //flips facing direction
+            facingDirection *= -1;
+            // *= -1 doesnt work here as you cant modify a single aspect of the local scale, so you have to modify all 3 
+            transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
 }
