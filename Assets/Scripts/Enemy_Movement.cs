305921869c184d7a1aeb3c1285c58e6a5034c105
Michal Romero <michal.romero02@gmail.com>
2026-08-16 14:25:15 +0100

    Add simple state machine, allow state changes for isIdle and isChasing

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -1,26 +1,36 @@
 using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Enemy_Movement : MonoBehaviour
 {
     public float speed;
-    private bool isChasing = true;
+
     //facing right
     private int facingDirection = 1;
 
+    [Header("Testing for enemy state")] // tracks current state
+    public EnemyState enemyState;
     private Rigidbody2D rb;
     public Transform player;
+    private Animator anim;
+
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        //Change the state to Chasing to begin the game
+        ChangeState(EnemyState.Chasing);
     }
 
     // Update is called once per frame
     void Update()
 {
-    if (isChasing)
+    if (enemyState == EnemyState.Chasing)
     {
         //if plaeyrs is on right side of enemy and enemy facing left, then flip
         if (player.position.x > transform.position.x && facingDirection == -1)
@@ -45,4 +55,42 @@ public class Enemy_Movement : MonoBehaviour
             // *= -1 doesnt work here as you cant modify a single aspect of the local scale, so you have to modify all 3 
             transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
+
+    
+    void ChangeState(EnemyState newState)
+    {
+        //Exit current animation
+        //sets the booleans (found in animator) to true or false depensing on state
+        if (enemyState == EnemyState.Chasing)
+        {
+            anim.SetBool("isChasing", false);
+        }
+        else if (enemyState == EnemyState.Idle)
+        {
+            anim.SetBool("isIdle", false);
+        }
+
+        //update current state
+        //sets new state to whatever was just passed in
+        enemyState = newState;
+
+        //Update new animation
+        //now that new state is set, we have to set the booleans to true or false depending on what it is
+        if (enemyState == EnemyState.Chasing)
+        {
+            anim.SetBool("isChasing", true);
+        }
+        else if (enemyState == EnemyState.Idle)
+        {
+            anim.SetBool("isIdle", true);
+        }
+
+    }
+}
+
+
+public enum EnemyState
+{
+    Idle,
+    Chasing,
 }
